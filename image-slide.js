44a7e0a5636b3081a52e301c2068306c69871fc9
timothyylim <timothylim23@gmail.com>
2024-11-13 11:22:20 +0100

    Move toggle logic to h5p-image-slide from h5p-image-slider

--- image-slide.js
+++ image-slide.js
@@ -9,6 +9,20 @@ H5P.ImageSlide = (function ($) {
     this.$ = $(this);
     H5P.EventDispatcher.call(this);
 
+    // Define checkOverflow as a method of the instance (we use CSS to hide the toggle button)
+    this.checkOverflow = function () {
+      if (self.$descriptionTextContainer && self.$descriptionTextContainer[0].scrollHeight > self.$descriptionTextContainer[0].clientHeight) {
+        self.$toggleButton.show();
+      } else if (self.$toggleButton) {
+        self.$toggleButton.hide();
+      }
+    };
+
+    // Listen resize events
+    this.on('resize', () => {
+      this.checkOverflow();
+    });
+
     this.aspectRatio = this.originalAspectRatio = extras.aspectRatio;
     // Extend defaults with provided options
     this.options = $.extend(true, {}, {
@@ -29,7 +43,6 @@ H5P.ImageSlide = (function ($) {
   C.prototype = Object.create(H5P.EventDispatcher.prototype);
   C.prototype.constructor = C;
 
-
   /**
    * Attach function called by H5P framework to insert H5P content into
    * page
@@ -39,8 +52,7 @@ H5P.ImageSlide = (function ($) {
   C.prototype.attach = function ($container) {
     this.$container = $container;
 
-    // Set class on container to identify it as a greeting card
-    // container.  Allows for styling later.
+    // Set class on container to identify it as an image slide container.
     $container.addClass("h5p-image-slide");
 
     this.$imageHolder = $('<div>', {
@@ -54,10 +66,81 @@ H5P.ImageSlide = (function ($) {
 
     // Add description if it exists
     if (this.description) {
-      $('<div>', {
+      // Create description element
+      const $description = $('<div>', {
         class: 'h5p-image-description',
-        text: this.description
       }).appendTo($container);
+
+      // Create toggle button within the description element and store it in the instance
+      this.$toggleButton = $('<div>', {
+        class: 'h5p-description-toggle',
+        'aria-label': 'Toggle description',
+        role: 'button',
+      }).appendTo($description).hide(); // Initially hide the toggle button
+
+      // Create an arrow icon inside the toggle button for independent rotation
+      this.$arrowIcon = $('<div>', {
+        class: 'arrow-icon',
+        css: {
+          'transform': 'rotate(180deg)'
+        }
+      }).appendTo(this.$toggleButton);
+
+      // Create the description text container and store it in the instance
+      this.$descriptionTextContainer = $('<div>', {
+        class: 'h5p-description-text-container',
+        text: this.description
+      }).appendTo($description);
+
+      // Set initial state for description visibility
+      $description.addClass('collapsed');
+      $description.css({
+        'display': '-webkit-box',
+        'max-height': '2.5em',
+        'overflow': 'visible',
+        '-webkit-box-orient': 'vertical',
+        '-webkit-line-clamp': 2,
+      });
+
+      // Run checkOverflow after the element is attached to the DOM
+      setTimeout(() => this.checkOverflow(), 0);
+
+      // Toggle function to expand/collapse description
+      const toggleDescription = () => {
+        const isCollapsed = $description.hasClass('collapsed');
+        $description.toggleClass('collapsed', !isCollapsed);
+        console.log('isCollapsed', isCollapsed);
+        if (isCollapsed) {
+          // Expand description
+          $description.css({
+            'display': 'block',
+            'max-height': 'none',
+            'overflow': 'visible',
+          });
+          this.$toggleButton.attr('aria-label', 'Collapse description');
+
+          // Rotate only the arrow icon when expanded
+          this.$arrowIcon.css({
+            'transform': 'rotate(0deg)',
+          });
+        } else {
+          // Collapse description
+          $description.css({
+            'display': '-webkit-box',
+            'max-height': '2.5em',
+          });
+          this.$toggleButton.attr('aria-label', 'Expand description');
+
+          // Reset the arrow icon rotation when collapsed
+          this.$arrowIcon.css({
+            'transform': 'rotate(180deg)',
+          });
+        }
+      };
+
+      // Bind the toggle function to both the toggle button and the description text
+      this.$toggleButton.on('click', toggleDescription);
+      this.$descriptionTextContainer.on('click', toggleDescription);
     }
 
     this.adjustSize();
